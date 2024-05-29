0eea4517ea10d830898940dd7e1fbba2f0254e00
JoShMiQueL <joshmiquel.cs@gmail.com>
2024-05-29 16:05:24 +0200

    feat(GameReader): add new GameReader class for handling game state
    This commit introduces a new GameReader class in GameReader.py, which is responsible for reading and monitoring the game state of Dark Souls Remastered. The new class encapsulates memory handling and state detection logic, which were previously spread across Main.py and utils.py.
    
    BREAKING CHANGE: The functions for reading game values and monitoring state changes have been removed from Main.py and utils.py, and are now part of the GameReader class. Main.py has been simplified to use the new GameReader class.

---
+++ GameReader.py
@@ -0,0 +1,77 @@
+import os
+import time
+from pymem import Pymem
+import pymem
+
+from utils import getPointerAddress
+
+class GameReader:
+  __memory: Pymem
+  __prev_state: dict = {}
+  __curr_state = __prev_state
+
+  def __init__(self):
+    try:
+      self.__memory = Pymem("DarkSoulsRemastered")
+    except pymem.exception.CouldNotOpenProcess:
+      print("Please run this script as administrator.")
+      os._exit(1)
+    except pymem.exception.ProcessNotFound:
+      print("Dark Souls Remastered is not running.")
+      os._exit(1)
+    except Exception as ex:
+      print(f"An error occurred: {ex}")
+      os._exit(1)
+  
+  def __read_state(self):
+    inGame = self.__read_value("inGame")
+    hp = self.__read_value("hp")
+    inLoadingScreen = self.__read_value("inLoadingScreen")
+    isAlive = self.__read_value("isAlive")
+
+    state = {
+      "inGame": inGame,
+      "inLoadingScreen": inLoadingScreen,
+      "hp": hp,
+    }
+
+    return state
+
+  def __read_value(self, value_name):
+    try:
+        if value_name == "hp":
+            baseAddress = self.__memory.base_address
+            value = self.__memory.read_int(getPointerAddress(self.__memory, baseAddress + 0x01ACD758, [0, 0x3E8]))
+            value = value if not value <= 0 else 0 # If hp has negative values return 0
+        elif value_name == "inGame":
+            baseAddress = self.__memory.base_address
+            value = self.__memory.read_bool(baseAddress + 0x1D260A8)
+        elif value_name == "inLoadingScreen":
+            value = self.__read_value("inGame") and (self.__read_value("hp") is None)
+        elif value_name == "isAlive":
+            value = self.__read_value("inGame") and (not self.__read_value("hp") <= 0)
+        else:
+            raise ValueError(f"Invalid value name: {value_name}")
+    except Exception as e:
+        # print(f"Error reading {value_name}: {e}")
+        value = None
+
+    return value
+  
+  def __detect_state_change(self, prev_state: dict) -> tuple:
+    current_state = self.__read_state()
+    if current_state != prev_state:
+        return current_state, True
+    else:
+        return current_state, False
+  
+  def __state_monitor(self):
+    while True:
+        self.__curr_state = self.__detect_state_change(self.__prev_state)
+        if (self.__curr_state[1] == True):
+            print("State has changed!", self.__curr_state[0])
+            self.__prev_state = self.__curr_state[0]
+        time.sleep(.01)
+  
+  def start(self):
+     self.__state_monitor()
